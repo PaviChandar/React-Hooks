7c238a5f13a1318a794f44b6ea116482dc216c6d
PaviChandar <pavithrachandar0501@gmail.com>
2022-05-10 17:07:32 +0530

    react-hooks

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import HooksCounter from './components/HooksCounter';
+import HooksDemo from './components/HooksDemo';
+import HooksCounterTwo from './components/HooksCounterTwo';
+import HooksArray from './components/HooksArray';
+import HooksEffect from './components/HooksEffect';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <HooksDemo /> */}
+      {/* <HooksCounter /> */}
+      {/* <HooksCounterTwo /> */}
+      {/* <HooksArray /> */}
+      <HooksEffect />
+    </div>
+  );
+}
+
+export default App;
